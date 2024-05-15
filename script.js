819a5a4ca6bbb6241caf93b4b6b050018db558b5
dzigi <zygisbuzas@gmail.com>
2024-05-15 10:30:30 +0300

    Last update

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-
 const pointsDisplay = document.getElementById('pointsDisplay')
 const gameContainer = document.getElementById('game')
-
 let points = 0
-
 
 for (let i = 0; i < 9; i++) {
     const square = document.createElement('div')
     square.className = 'square'
     square.id = `${i}`
     gameContainer.append(square)
-    setInterval(function (){
-        const randomNumber = Math.floor(Math.random() * 9)
+}
+
+const squares = document.querySelectorAll('.square')
+
+setInterval(function () {
+    const randomNumber = Math.floor(Math.random() * 9)
+    squares.forEach((square, index) => {
         if (parseInt(square.id) === randomNumber) {
             square.style.backgroundColor = 'green'
+            setTimeout(function () {
+                square.style.backgroundColor = 'white'
+            }, 500)
         }
-        setTimeout(function (){
-            square.style.backgroundColor = 'white'
-        },500)
-    },1000)
+    });
+}, 1000)
 
-    square.onclick = function (){
-        if (square.style.backgroundColor === 'green'){
-            points++
+squares.forEach(square => {
+    square.onclick = function () {
+        if (square.style.backgroundColor === 'green') {
+            points++;
             pointsDisplay.innerHTML = `Points: ${points}`
         }
     }
-}
+});
