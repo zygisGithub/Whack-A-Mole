fac7603da1caf2cf7a1438d8dbfff875269d6cc6
dzigi <zygisbuzas@gmail.com>
2024-05-15 10:08:49 +0300

    'update2'

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+
 const pointsDisplay = document.getElementById('pointsDisplay')
 const gameContainer = document.getElementById('game')
 
 let points = 0
 
-function updatePoints () {
-    pointsDisplay.innerHTML = `Points: ${points}`
-}
 
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 9; i++) {
     const square = document.createElement('div')
     square.className = 'square'
+    square.id = `${i}`
     gameContainer.append(square)
+    console.log(square.id)
+    setInterval(function (){
+        const randomNumber = Math.floor(Math.random() * 9)
+        if (randomNumber === Number(square.id)) {
+            square.style.backgroundColor = 'green'
+        }
+        setTimeout(function (){
+            square.style.backgroundColor = 'white'
+        },400)
+    },500)
+
+    square.onclick = function (){
+        if (square.style.backgroundColor === 'green'){
+            points++
+            pointsDisplay.innerHTML = `Points: ${points}`
+        }
+    }
 }
